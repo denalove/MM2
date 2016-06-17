7212f73a41ef7185f9ea3059d9128c715e3c9ffa
Dena Sawyer <denasawyer1111@gmail.com>
2016-06-16 23:51:57 -0400

    update versions

--- config/config.js
+++ config/config.js
@@ -64,7 +64,7 @@ var config = {
         //Set of modules which should be shown for every user
         forAllClass: "for_all"
     	}
-		},*/
+		},
 		{
     module: 'mm-music-player',
     position: 'bottom_left',
@@ -72,7 +72,7 @@ var config = {
         startingVolume: 20,
         fadeDuriation: 2000
     	}
-		},
+		},*/
 		{
 			module: 'currentweather',
 			position: 'top_right',
